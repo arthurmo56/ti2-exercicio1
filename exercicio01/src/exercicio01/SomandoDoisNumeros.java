cf569cb274c1c6510474bf9c49034e27e251279e
arthurmo56 <130711686+arthurmo56@users.noreply.github.com>
2023-08-22 20:03:26 -0300

    Update SomandoDoisNumeros.java

--- exercicio01/src/exercicio01/SomandoDoisNumeros.java
+++ exercicio01/src/exercicio01/SomandoDoisNumeros.java
@@ -3,16 +3,20 @@ import java.util.*;
  class SomandoDoisNumeros {
 	public static Scanner sc = new Scanner(System.in);
 	 public static void main(String args[]) {
-		
+
+		 //Declaração das variaveis
 		 int n1, n2, soma;
-		 
+
+		 //Leitura dos numeros
 		 System.out.println("Digite um numero");
 		 n1= sc.nextInt();
 		 System.out.println("Digite outro numero");
 		 n2= sc.nextInt();
-		 
+
+		 //Efetuar a soma
 		 soma = n1+n2;
-		 
+
+		 // Exibir o resultado
 		 System.out.println("Soma: " + soma);
 	 }
 }
